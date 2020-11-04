af341fc30cb0a55902ac6fcfd2200bf145e27869
genius-jo <joks7285@daum.net>
2020-11-04 19:41:18 +0900

    rgb 추출

--- app/src/main/java/com/example/richercolor/rgbtest.java
+++ app/src/main/java/com/example/richercolor/rgbtest.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.PorterDuff;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 
 public class rgbtest extends AppCompatActivity {
 
@@ -23,6 +29,7 @@ public class rgbtest extends AppCompatActivity {
     private SeekBar seekBarblue;
 
     Button button, button2, button3;
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,8 @@ public class rgbtest extends AppCompatActivity {
         button = findViewById(R.id.btn_color_change);
         button2 = findViewById(R.id.btn_color_change2);
         button3 = findViewById(R.id.btn_color_change3);
+        button4 = findViewById(R.id.btn_color_change4);
+        /*
 
         seekBarred.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
@@ -45,6 +54,7 @@ public class rgbtest extends AppCompatActivity {
                 int green = seekBargreen.getProgress();
                 int blue = seekBarblue.getProgress();
                 imgtest.setColorFilter(Color.rgb(red,green,blue),PorterDuff.Mode.LIGHTEN);
+
             }
 
             @Override
@@ -116,14 +126,56 @@ public class rgbtest extends AppCompatActivity {
             }
         });
 
+
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ColorMatrix result = new ColorMatrix(ColorMatrices.SWAP_G_B);
                 imgtest.setColorFilter(new ColorMatrixColorFilter(result));
             }
-        });
+        });*/
 
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                imgtest.setImageResource(R.drawable.cow);
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.cow);
+
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+
+                int size = w*h;
+
+                int[] pixels = new int[size];
+
+                bitmap.getPixels(pixels, 0, w, 0, 0, w, h);
+
+                /*
+                Log.d("rgbtest", ""+pixels.length);
+                for (int i=0; i<pixels.length; i++) {
+                    Log.d("rgbtest", "pixels"+i + " " + pixels[i]);
+                }*/
+
+                for(int i=0; i<size; i++)
+                {
+                    int color = pixels[i];
+
+                    int r = (color >> 16) & 0xFF;
+                    int g = (color >> 8) & 0xFF;
+                    int b = (color) & 0xFF;
+
+                    Log.d("rgbtest", "r"+i + " " + r);
+                    Log.d("rgbtest", "g"+i + " " + g);
+                    Log.d("rgbtest", "b"+i + " " + b);
+
+                    Log.d("rgbtest", "pixels"+i + " " + color);
+
+                }
+                imgtest.setImageBitmap(bitmap);
+
+            }
+        });
 
 
     }
