4698a7e1f7ea9256b8e2eec2d40af257d1ef1b47
genius-jo <joks7285@daum.net>
2020-11-07 05:02:26 +0900

    적록색맹 부분 오류 수정, 적록색맹 부분 기능 완성

--- app/src/main/java/com/example/richercolor/Album.java
+++ app/src/main/java/com/example/richercolor/Album.java
@@ -211,6 +211,7 @@ public class Album extends AppCompatActivity {
                     {
                         int colorA = extract_pixelsA[i];
 
+                        int aA = (colorA >> 24) & 0xFF;
                         int rA = (colorA >> 16) & 0xFF;
                         int gA = (colorA >> 8) & 0xFF;
                         int bA = (colorA) & 0xFF;
@@ -222,10 +223,9 @@ public class Album extends AppCompatActivity {
 
                         else //검은색이 아닐때(추출한 색일때), 빨간색의 크기를 키움
                         {
-                            int change_color = colorA;
                             //change_color |= 255 << 16;
 
-                            int change_red = (change_color >> 16);
+                            int change_red = rA;
 
                             int change_red_plus_range = 255-change_red;
 
@@ -236,8 +236,9 @@ public class Album extends AppCompatActivity {
                             if(change_red >= 255)
                                 change_red = 255;
 
+                            int change_color = 0;
+                            change_color |= aA << 24;
                             change_color |= change_red << 16;
-
                             change_color |= gA << 8;
                             change_color |= bA;
 
@@ -249,6 +250,7 @@ public class Album extends AppCompatActivity {
                     {
                         int colorB = extract_pixelsB[i];
 
+                        int aB = (colorB >> 24) & 0xFF;
                         int rB = (colorB >> 16) & 0xFF;
                         int gB = (colorB >> 8) & 0xFF;
                         int bB = (colorB) & 0xFF;
@@ -264,9 +266,7 @@ public class Album extends AppCompatActivity {
 
                         else //검은색이 아닐때(추출한 색일때), 초록색의 크기를 키움
                         {
-                            int change_color = colorB;
-
-                            int change_green = (change_color >> 8);
+                            int change_green = gB;
 
                             int change_green_plus_range = 255 - change_green;
 
@@ -277,9 +277,13 @@ public class Album extends AppCompatActivity {
                             if(change_green >= 255)
                                 change_green = 255;
 
+                            int change_color = 0;
+
+                            change_color |= aB << 24;
                             change_color |= rB << 16;
                             change_color |= change_green << 8;
                             change_color |= bB;
+
                             changed_pixels[i] = change_color;
                         }
                     }
@@ -334,103 +338,21 @@ public class Album extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                button_cnt++;
+                if(button_cnt < 3)
+                    button_cnt++;
 
-                if(button_cnt <= 4) {
+                if(button_cnt <= 3) {
                     if (clicked_button_original == 1) {
                         //아무것도 안함
                     }
 
                     if (clicked_button_redgreen == 1) {
-                        //show_image.setImageResource(R.drawable.cow);
-                        //BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
-                        //Bitmap bitmap2 = drawable.getBitmap();
-                        //Bitmap bitmapA = drawable.getBitmap();
-                        //Bitmap bitmapB = drawable.getBitmap();
-
-                        // Bitmap bitmap2 = resizeBitmap(1024, bitmap); // 이미지 크기 조정
-
-                        //int w = bitmap2.getWidth(); //원본의 w사이즈
-                        //int h = bitmap2.getHeight(); //원본의 h사이즈
-                       // int size = w * h; //원본 사이즈
-                        /*
-                        real_pixels = new int[size]; //원본 픽셀
-                        extract_pixelsA = new int[size]; //추출된 색상 픽셀
-                        extract_pixelsB = new int[size]; //추출된 색상 픽셀
-                        changed_pixels = new int[size]; //최종적으로 바꿀 픽셀
-                        plus_rangeA1 = new int[size]; //플러스 하면 올라가는 크기 1단계
-                        minus_rangeA1 = new int[size];//마이너스 할때 올라가는 크기
-                        plus_rangeB1 = new int[size]; //플러스 하면 올라가는 크기
-                        minus_rangeB1 = new int[size];//마이너스 할때 올라가는 크기
-                        plus_rangeA2 = new int[size]; //플러스 하면 올라가는 크기 2단계
-                        minus_rangeA2 = new int[size];//마이너스 할때 올라가는 크기
-                        plus_rangeB2 = new int[size]; //플러스 하면 올라가는 크기
-                        minus_rangeB2 = new int[size];//마이너스 할때 올라가는 크기
-                        plus_rangeA3 = new int[size]; //플러스 하면 올라가는 크기 3단계
-                        minus_rangeA3 = new int[size];//마이너스 할때 올라가는 크기
-                        plus_rangeB3 = new int[size]; //플러스 하면 올라가는 크기
-                        minus_rangeB3 = new int[size];//마이너스 할때 올라가는 크기
-                        */
-
-                        //bitmap2.getPixels(real_pixels, 0, w, 0, 0, w, h); //real_pixels에 원본 픽셀을 넣음
-
-                        // bitmapA를 Mat 객체로 변환
-                        /*
-                        Mat originalA = new Mat (bitmapA.getWidth(), bitmapA.getHeight(), CvType.CV_8UC3);
-                        Utils.bitmapToMat(bitmapA, originalA);
-
-                        // bitmapB를 Mat 객체로 변환
-                        Mat originalB = new Mat (bitmapB.getWidth(), bitmapB.getHeight(), CvType.CV_8UC3);
-                        Utils.bitmapToMat(bitmapB, originalB);
-
-                        // rgb를 hsv 변환
-                        Mat hsv_originalA = new Mat();
-                        Imgproc.cvtColor(originalA, hsv_originalA, Imgproc.COLOR_RGB2HSV);
-
-                        Mat hsv_originalB = new Mat();
-                        Imgproc.cvtColor(originalB, hsv_originalB, Imgproc.COLOR_RGB2HSV);
-
-                        // 빨간색 영역을 구하는 마스크 생성
-                        Mat mask = new Mat();
-                        Core.inRange(hsv_originalA, lower_red, upper_red, mask);
-
-                        // 초록색 영역을 구하는 마스크 생성
-                        Mat mask2 = new Mat();
-                        Core.inRange(hsv_originalB, lower_green, upper_green, mask2);
-
-                        // 빨간색 영역만 구한 사진
-                        Mat changeA = new Mat();
-                        Core.bitwise_and(hsv_originalA, hsv_originalA, changeA, mask);
-
-                        // 초록색 영역만 구한 사진
-                        Mat changeB = new Mat();
-                        Core.bitwise_and(hsv_originalB, hsv_originalB, changeB, mask2);
-
-                        // 빨간색 영역을 제외한 사진
-                        Mat unChangeA = new Mat();
-                        Core.bitwise_xor(hsv_originalA, changeA, unChangeA);
-
-                        // 초록색 영역을 제외한 사진
-                        Mat unChangeB = new Mat();
-                        Core.bitwise_xor(hsv_originalB, changeB, unChangeB);
-
-                        // result 그림을 다시 rgb로 바꿈
-                        Imgproc.cvtColor(changeA, changeA, Imgproc.COLOR_HSV2RGB);
-                        Imgproc.cvtColor(changeB, changeB, Imgproc.COLOR_HSV2RGB);
-
-                        // Mat 객체를 Bitmap 객체로 바꿈
-                        Utils.matToBitmap(changeA, bitmapA);
-                        Utils.matToBitmap(changeB, bitmapB);
-
-                        bitmapA.getPixels(extract_pixelsA, 0, w, 0, 0, w, h); //extract_pixels에는 추출된 색상이 저장, 추출되지 않은 색상은 검정색으로
-                        bitmapB.getPixels(extract_pixelsB, 0, w, 0, 0, w, h); //extract_pixels에는 추출된 색상이 저장, 추출되지 않은 색상은 검정색으로
-
-                         */
                         /*******************************************************/
                         for(int i=0; i<size; i++)
                         {
                             int colorA = extract_pixelsA[i];
 
+                            int aA = (colorA >> 24) & 0xFF;
                             int rA = (colorA >> 16) & 0xFF;
                             int gA = (colorA >> 8) & 0xFF;
                             int bA = (colorA) & 0xFF;
@@ -442,18 +364,18 @@ public class Album extends AppCompatActivity {
 
                             else //검은색이 아닐때(추출한 색일때), 빨간색의 크기를 키움
                             {
-                                int change_color = colorA;
-                                //change_color |= 255 << 16;
 
-                                int change_red = (change_color >> 16) & 0xFF;
+                                int change_red = rA;
 
                                 change_red = change_red + rangeA[i]*button_cnt;
 
                                 if(change_red >= 0xFF)
                                     change_red = 0xFF;
 
-                                change_color = change_red << 16;
+                                int change_color = 0;
 
+                                change_color |= aA << 24;
+                                change_color |= change_red << 16;
                                 change_color |= gA << 8;
                                 change_color |= bA;
 
@@ -465,6 +387,7 @@ public class Album extends AppCompatActivity {
                         {
                             int colorB = extract_pixelsB[i];
 
+                            int aB = (colorB >> 24) & 0xFF;
                             int rB = (colorB >> 16) & 0xFF;
                             int gB = (colorB >> 8) & 0xFF;
                             int bB = (colorB) & 0xFF;
@@ -481,13 +404,15 @@ public class Album extends AppCompatActivity {
                             else //검은색이 아닐때(추출한 색일때), 초록색의 크기를 키움
                             {
                                 int change_green = gB;
-                                int change_color = 0;
 
                                 change_green = change_green + (rangeB[i]*button_cnt);
 
                                 if(change_green >= 0xFF)
                                     change_green = 0xFF;
 
+                                int change_color = 0;
+
+                                change_color |= aB << 24;
                                 change_color |= rB << 16;
                                 change_color |= change_green << 8;
                                 change_color |= bB;
@@ -512,6 +437,97 @@ public class Album extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                if(button_cnt > 0)
+                    button_cnt--;
+
+                if(button_cnt >= 0) {
+                    if (clicked_button_original == 1) {
+                        //아무것도 안함
+                    }
+
+                    if (clicked_button_redgreen == 1) {
+                        /*******************************************************/
+                        for(int i=0; i<size; i++)
+                        {
+                            int colorA = extract_pixelsA[i];
+
+                            int aA = (colorA >> 24) & 0xFF;
+                            int rA = (colorA >> 16) & 0xFF;
+                            int gA = (colorA >> 8) & 0xFF;
+                            int bA = (colorA) & 0xFF;
+
+                            if(rA==0x00 && gA==0x00 && bA==0x00) //검정색일때
+                            {
+                                changed_pixels[i] = real_pixels[i];
+                            }
+
+                            else //검은색이 아닐때(추출한 색일때), 빨간색의 크기를 키움
+                            {
+
+                                int change_red = rA;
+
+                                change_red = change_red + rangeA[i]*button_cnt;
+
+                                if(change_red >= 0xFF)
+                                    change_red = 0xFF;
+
+                                int change_color = 0;
+
+                                change_color |= aA << 24;
+                                change_color |= change_red << 16;
+                                change_color |= gA << 8;
+                                change_color |= bA;
+
+                                changed_pixels[i] = change_color;
+                            }
+                        }
+
+                        for(int i=0; i<size; i++)
+                        {
+                            int colorB = extract_pixelsB[i];
+
+                            int aB = (colorB >> 24) & 0xFF;
+                            int rB = (colorB >> 16) & 0xFF;
+                            int gB = (colorB >> 8) & 0xFF;
+                            int bB = (colorB) & 0xFF;
+
+                            if(rB==0x00 && gB==0x00 && bB==0x00) //검정색일때
+                            {
+                                if(changed_pixels[i] != real_pixels[i]) //빨간색에서 바뀐것일때
+                                    continue;
+
+                                else
+                                    changed_pixels[i] = real_pixels[i];
+                            }
+
+                            else //검은색이 아닐때(추출한 색일때), 초록색의 크기를 키움
+                            {
+                                int change_green = gB;
+
+                                change_green = change_green + (rangeB[i]*button_cnt);
+
+                                if(change_green >= 0xFF)
+                                    change_green = 0xFF;
+
+                                int change_color = 0;
+
+                                change_color |= aB << 24;
+                                change_color |= rB << 16;
+                                change_color |= change_green << 8;
+                                change_color |= bB;
+
+                                changed_pixels[i] = change_color;
+                            }
+                        }
+                        /******************************************/
+                        bitmap2.setPixels(changed_pixels,0, w, 0, 0, w, h);
+                        show_image.setImageBitmap(bitmap2);
+                    }
+
+                    if (clicked_button_yellowblue == 1) {
+
+                    }
+                }
 
 
             }
